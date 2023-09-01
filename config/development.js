b0ad4ed9207a5c35fb8de69d1355314240040377
sasidharan10 <ksasidharan98@gmail.com>
2023-09-01 12:30:04 +0530

    config fix

--- config/development.js
+++ config/development.js
@@ -1,14 +1,10 @@
 require('dotenv').config();
 
-let tempSC = true;
-if (process.env.SECURE_COOKIE_DEV === 'true')
-    tempSC = true;
-else
-    tempSC = false;
+const secureCookieVal = false;
 
 module.exports = {
     dbUrl: process.env.MONGO_DB_DEV,
     secret: process.env.SECRET_DEV,
     port: process.env.PORT || 5000,
-    secureCookie: tempSC
+    secureCookie: secureCookieVal
 }
--- config/production.js
+++ config/production.js
@@ -1,14 +1,10 @@
 require('dotenv').config();
 
-let tempSC = true;
-if (process.env.SECURE_COOKIE_DEV === 'true')
-    tempSC = true;
-else
-    tempSC = false;
+const secureCookieVal = true;
 
 module.exports = {
-    dbUrl : process.env.MONGO_DB_PROD,
+    dbUrl: process.env.MONGO_DB_PROD,
     secret: process.env.SECRET_PROD,
     port: process.env.PORT || 5000,
-    secureCookie: tempSC
+    secureCookie: secureCookieVal
 }
